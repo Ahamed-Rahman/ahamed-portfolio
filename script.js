dde2e70eae15cdc0260dc1c6ed35b7c03bce4a42
ahamed0720 <it22125170@my.sliit.lk>
2024-12-07 13:29:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional smooth scroll functionality
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
